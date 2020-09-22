900be7559a4ca13ca7c82e1ed2c599959c91b602
Domenico Capalbo <capalbodomenico@gmail.com>
2020-09-22 17:29:38 +0200

    refactory
    ajax-ex-boolflix

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
 $(document).ready(function() {
    //make a function to show the films data when writing inside the input
-   $(".searchbar").click(function(){
+   $(".search_button").click(function(){
      //clean the val inside the input
-     $(this).val("");
+     $(".films_input").val("");
+     // takes the input value
+     var searchFilms = $(".films_input").val();
+     //invoke the function to print the data
+     PrintMoviesData(searchFilms);
+     // clean the input again
+     $(".films_input").val();
    });
    //make a variable for the Api and the ApiKey
-   var api = "https://api.themoviedb.org/3/search/movie";
-   var apiKey = "765dd26370a4e82d38aa3ee95f3f61e9";
 });
    //FUNCTION
-   function PrintMoviesData(films) {
-     var source = $("#movies-template")
-     console.log(source);
-     var template = Handlebars.compile(source);
-     console.log(template);
+   function PrintMoviesData(searchFilms) {
+    //make the handlebars variables
+    var source = $("#movies_template").html();
+    var template = Handlebars.compile(source);
+    var api = "https://api.themoviedb.org/3/search/movie";
+    var apiKey = "765dd26370a4e82d38aa3ee95f3f61e9";
    //make an ajax call
    $.ajax(
     {
@@ -27,24 +32,23 @@ $(document).ready(function() {
       "success": function(data) {
         var films = data.response;
         console.log(films);
-     },
+        //make a cicle for to get inside of the api Array objects
+        for (var i = 0; i < films.length; i++) {
+           var context = {
+            //make some property which are going to be inserted in the placeholders
+            "title": films[i].title,
+            "originaltitle": films[i].original_title,
+            "language": films[i].original_language,
+            "vote": films[i].vote_average
+          };
+          //make an html variable with the handlebars context
+          var html = template(context);
+          //append the html inside the DOM
+          $("#movies_list").append(html);
+        }
+      },
        "error": function (err) {
         alert("There is an error with the Ajax call. "+ err);
-
-       //make a cicle for to get inside of the api Array objects
-       for (var i = 0; i < films.length; i++) {
-          var context = {
-           //make some property which are going to be inserted in the placeholders
-           title: films.title,
-           originaltitle: films.original_title,
-           language: films.original_language,
-           vote: films.vote_average
-         };
-         //make an html variable with the handlebars context
-         var html = template(context);
-         //append the html inside the DOM
-         $("#movies_list").append(html);
-       }
      }
-   })
- };
+  });
+};
