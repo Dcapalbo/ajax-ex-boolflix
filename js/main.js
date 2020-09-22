4e9acc96abfbb7e81fd063b11576e68bf0594a1f
Domenico Capalbo <capalbodomenico@gmail.com>
2020-09-22 15:31:40 +0200

    make-function-for-find-data-from-api
    ajax-ex-boolflix

--- js/main.js
+++ js/main.js
@@ -1,13 +1,32 @@
-// Milestone 1
-// Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il  bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
-// Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
-// Titolo
-// Titolo Originale
-// Lingua
-// Voto
 $(document).ready(function() {
 
-   //make a variable for the API
-   var api = "https://api.themoviedb.org/3/search/movie?api_key=765dd26370a4e82d38aa3ee95f3f61e9&language=en-US&query=blade%20runner&page=1&include_adult=false";
-
+//FUNCTION
+var api = "https://api.themoviedb.org/3/search/movie?api_key=765dd26370a4e82d38aa3ee95f3f61e9&language=en-US&query=blade%20runner&page=1&include_adult=false";
+var source = $("#movies-template")
+var template = Handlebars.compile(source);
+function renderMoviesData(data) {
+  //make a variable for the API
+  //make an ajax call
+  $.ajax(
+   {
+     "url": api,
+     "method": "GET",
+     "success": function(data) {
+       console.log(data);
+       for (var i = 0; i < data.length; i++) {
+         var context = {
+          title: data.title,
+          originaltitle: data.originaltitle,
+          language: data.language,
+          vote: data.vote
+         };
+         var html = template(context);
+         $("#movies_list").append(html);
+       }
+    },
+      "error": function (err) {
+       alert("There is an error with the Ajax call. "+ err);
+      }
+    });
+ }
 });
