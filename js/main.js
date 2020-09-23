92dd40f6cdf28c5b20bc59bbe17b8a723f39e144
Domenico Capalbo <capalbodomenico@gmail.com>
2020-09-23 22:18:55 +0200

    milestone-2-complete
    ajax-ex-boolflix

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
-//   Milestone 2:
-// Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5, lasciando le restanti vuote (troviamo le icone in FontAwesome).
-// Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze piene (o mezze vuote :P)
-// Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
-
+   //make a function which at the press enter will insert the data
    $(".films_input").keypress(function() {
     if (event.which == 13) {
       // make a variable which takes the value of the Movies inputs
@@ -17,7 +13,6 @@ $(document).ready(function() {
       PrintSeriesData(printSeries);
     }
    });
-
    //make a function to show the films data when writing inside the input
    $(".search_button").click(function(){
      // make a variable which takes the value of the Movies inputs
@@ -30,15 +25,14 @@ $(document).ready(function() {
      PrintMoviesData(printMovies);
      PrintSeriesData(printSeries);
    });
-
 });
    //FUNCTIONS
    //make a function which conver the votes in stars
-   function convert(vote) {
+   function conversionVote(vote) {
      //make a variable to fix and divide the vote numbers
      var newVote = Math.ceil(vote / 2);
      //make the handlebars variables
-     var source = $("#stars-template").html();
+     var source = $("#stars_template").html();
      var template = Handlebars.compile(source);
      var html = template();
      var stars = "";
@@ -47,6 +41,16 @@ $(document).ready(function() {
        stars += html;
      }
      return stars;
+   }
+   //make a function to control the presence of the language inside the site and to change it where it's usefull
+   function conversionLanguage(language) {
+     if (language == "en") {
+       return "gb";
+     } else if (language == "ja") {
+       return "jp";
+     } else {
+       return language;
+     }
    }
    //make a function to print movies Data inside the DOM
    function PrintMoviesData(printMovies) {
@@ -68,19 +72,23 @@ $(document).ready(function() {
       "method": "GET",
       "success": function(data) {
         var results = data.results;
+        //invoke the language function
+        conversionLanguage(originaLanguage);
         //make a cicle for to get inside of the api Array objects
         for (var i = 0; i < results.length; i++) {
            var context = {
             //make some property which are going to be inserted in the placeholders
             "title": results[i].title,
             "originaltitle": results[i].original_title,
-            "language": results[i].original_language,
-            "vote": results[i].vote_average
-          };
-          // invoke the conver function
-          convert(vote);
+            "vote": results[i].vote_average,
+            "original_language": originaLanguage
+          }
+          //make a variable for the new context value
+          var originaLanguage = results[i].original_language;
+          //invoke the conversionVote function
+          conversionVote(vote);
           //make a variable to take the vote results and convert it in stars value
-          var vote = convert(results[i].vote_average);
+          var vote = conversionVote(results[i].vote_average);
           $("#movies_list").append(vote);
           //make an html variable with the handlebars context
           var html = template(context);
@@ -113,19 +121,23 @@ $(document).ready(function() {
       "method": "GET",
       "success": function(data) {
         var results = data.results;
+        //invoke the language function
+        conversionLanguage(originaLanguage);
         //make a cicle for to get inside of the api Array objects
         for (var i = 0; i < results.length; i++) {
            var context = {
             //make some property which are going to be inserted in the placeholders
             "name": results[i].name,
             "originalname": results[i].original_name,
-            "language": results[i].original_language,
-            "vote": results[i].vote_average
+            "vote": results[i].vote_average,
+            "original_language": originaLanguage
           };
-          // invoke the convert function
-          convert(vote);
+          //make a variable for the new context value
+          var originaLanguage = results[i].original_language;
+          // invoke the conversionVote function
+          conversionVote(vote);
           //make a variable to take the vote results and convert it in stars value
-          var vote = convert(results[i].vote_average);
+          var vote = conversionVote(results[i].vote_average);
           $("#series_list").append(vote);
           //make an html variable with the handlebars context
           var html = template(context);
@@ -137,9 +149,9 @@ $(document).ready(function() {
         alert("There is an error with the Ajax call. "+ err);
      }
    });
-   };
+};
    //make a function to clear the ul inside the DOM
    function Clear() {
      $("#movies_list li").remove();
      $("#series_list li").remove();
-   };
+};
