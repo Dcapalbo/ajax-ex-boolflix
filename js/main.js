3ba011c23a8e782ca0272d44105c5e1412d3f43c
Domenico Capalbo <capalbodomenico@gmail.com>
2020-09-23 17:41:55 +0200

    milestone-2-stars-done
    ajax-ex-boolflix

--- js/main.js
+++ js/main.js
@@ -3,10 +3,7 @@ $(document).ready(function() {
 // Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5, lasciando le restanti vuote (troviamo le icone in FontAwesome).
 // Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze piene (o mezze vuote :P)
 // Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
-   function transformNumb(number) {
-     var newVote = series[i].vote_average;
-     newVote = Math.ceil(number / 2);
-   }
+
    $(".films_input").keypress(function() {
     if (event.which == 13) {
       // make a variable which takes the value of the Movies inputs
@@ -36,6 +33,21 @@ $(document).ready(function() {
 
 });
    //FUNCTIONS
+   //make a function which conver the votes in stars
+   function convert(vote) {
+     //make a variable to fix and divide the vote numbers
+     var newVote = Math.ceil(vote / 2);
+     //make the handlebars variables
+     var source = $("#stars-template").html();
+     var template = Handlebars.compile(source);
+     var html = template();
+     var stars = "";
+
+     for (var i = 0; i < newVote; i++){
+       var stars += html;
+     }
+     return stars;
+   }
    //make a function to print movies Data inside the DOM
    function PrintMoviesData(printMovies) {
     //make the handlebars variables
@@ -55,16 +67,21 @@ $(document).ready(function() {
      },
       "method": "GET",
       "success": function(data) {
-        var films = data.results;
+        var results = data.results;
         //make a cicle for to get inside of the api Array objects
-        for (var i = 0; i < films.length; i++) {
+        for (var i = 0; i < results.length; i++) {
            var context = {
             //make some property which are going to be inserted in the placeholders
-            "title": films[i].title,
-            "originaltitle": films[i].original_title,
-            "language": films[i].original_language,
-            "vote": films[i].vote_average
+            "title": results[i].title,
+            "originaltitle": results[i].original_title,
+            "language": results[i].original_language,
+            "vote": results[i].vote_average
           };
+          // invoke the conver function
+          convert(vote);
+          //make a variable to take the vote results and convert it in stars value
+          var vote = convert(results[i].vote_average);
+          $("#movies_list").append(vote);
           //make an html variable with the handlebars context
           var html = template(context);
           //append the html inside the DOM
@@ -95,17 +112,21 @@ $(document).ready(function() {
      },
       "method": "GET",
       "success": function(data) {
-        var series = data.results;
-        console.log(series);
+        var results = data.results;
         //make a cicle for to get inside of the api Array objects
-        for (var i = 0; i < series.length; i++) {
+        for (var i = 0; i < results.length; i++) {
            var context = {
             //make some property which are going to be inserted in the placeholders
-            "name": series[i].name,
-            "originalname": series[i].original_name,
-            "language": series[i].original_language,
-            "vote": series[i].vote_average
+            "name": results[i].name,
+            "originalname": results[i].original_name,
+            "language": results[i].original_language,
+            "vote": results[i].vote_average
           };
+          // invoke the convert function
+          convert(vote);
+          //make a variable to take the vote results and convert it in stars value
+          var vote = convert(results[i].vote_average);
+          $("#series_list").append(vote);
           //make an html variable with the handlebars context
           var html = template(context);
           //append the html inside the DOM
