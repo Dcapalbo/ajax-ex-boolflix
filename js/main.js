025b50ba24ca28b328faacddd9ab622509aafcbb
Domenico Capalbo <capalbodomenico@gmail.com>
2020-09-23 13:52:33 +0200

    working-milestone-2-keypress-function-added

--- js/main.js
+++ js/main.js
@@ -1,12 +1,4 @@
 $(document).ready(function() {
-//   Milestone 2:
-// Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5, lasciando le restanti vuote (troviamo le icone in FontAwesome).
-// Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze piene (o mezze vuote :P)
-// Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
-// Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
-// Qui un esempio di chiamata per le serie tv:
-// https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=scrubs
-
    //make a function to show the films data when writing inside the input
    $(".search_button").click(function(){
      // takes the input value
@@ -17,13 +9,25 @@ $(document).ready(function() {
      //invoke the function to print the data
      PrintMoviesData(printMovies);
    });
-   //make a variable for the Api and the ApiKey
+
+   $(".films_input").keypress(function() {
+    if (event.which == 13) {
+      // takes the input value
+      var printMovies = $(".films_input").val();
+      $(".films_input").val("");
+      // Invoke a function to clear the DOM
+      Clear();
+      //invoke the function to print the data
+      PrintMoviesData(printMovies);
+    }
+   });
 });
    //FUNCTION
    function PrintMoviesData(printMovies) {
     //make the handlebars variables
     var source = $("#movies_template").html();
     var template = Handlebars.compile(source);
+    //make a variable for the Api and the ApiKey
     var api = "https://api.themoviedb.org/3/search/movie";
     var apiKey = "765dd26370a4e82d38aa3ee95f3f61e9";
    //make an ajax call
