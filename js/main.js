7ad2ef24937c24993ccf099d0a7d6b6eff241044
Domenico Capalbo <capalbodomenico@gmail.com>
2020-10-15 22:44:38 +0200

    milestone-4-refactory
    ajax-ex-boolflix

--- js/main.js
+++ js/main.js
@@ -6,9 +6,10 @@ $(document).ready(function(){
      var printValue = $(".films_input").val();
      $(".films_input").val("");
      // Invoke a function to clear the DOM
-     Clear();
+     Reset();
      //invoke the function to print movies and series data inside the DOM
-     PrintInputsData(printValue);
+     PrintInputsData(printValue, "movie");
+     PrintInputsData(printValue, "tv");
    }
  });
   //make an event function to show the films data when writing inside the input
@@ -17,9 +18,10 @@ $(document).ready(function(){
     var printValue = $(".films_input").val();
     $(".films_input").val("");
     // Invoke a function to clear the DOM
-    Clear();
+    Reset();
     //invoke the function to print movies and series data inside the DOM
-    PrintInputsData(printValue);
+    PrintInputsData(printValue, "movie");
+    PrintInputsData(printValue, "tv");
   });
 });
   //FUNCTIONS
@@ -32,11 +34,11 @@ $(document).ready(function(){
     var emptyStar ="<i class='far fa-star'></i>";
     var stars = "";
     // make a cicle "for" to create the stars
-    for (var i = 0; i <= 5; i++){
+    for (var i = 0; i < 5; i++){
       if (i <= newVote) {
-        var star = fullStar;
+        stars += fullStar;
       } else {
-        var star = emptyStar;
+        stars += emptyStar;
       }
     }
     // make a return of stars
@@ -44,7 +46,7 @@ $(document).ready(function(){
   }
   //make a function which takes the
   //make a function to clear the ul inside the DOM
-  function Clear() {
+  function Reset() {
     $("#movies_list li, h1, h3").remove();
     $("#series_list li, h1, h3").remove();
   }
@@ -58,20 +60,20 @@ $(document).ready(function(){
   return image
  }
   //make a function to print movies Data inside the DOM
-  function PrintInputsData(search) {
+  function PrintInputsData(search, type) {
    //make the handlebars variables
+   console.log(type);
    var source = $("#movies_template").html();
    var template = Handlebars.compile(source);
    //make a variable for the Api and the ApiKey
    var apiKey = "765dd26370a4e82d38aa3ee95f3f61e9";
-   //make an ajax call for the series
+   //make an ajax call for movies and series
    $.ajax(
      {
-      "url": "https://api.themoviedb.org/3/search/tv",
-      "data":{
+      "url": "https://api.themoviedb.org/3/search/" + type,
+      "data": {
        "api_key": apiKey,
-       "query": search,
-       "page": "1",
+       "query": search
      },
       "method": "GET",
       "success": function(data) {
@@ -85,7 +87,7 @@ $(document).ready(function(){
          "overview": results[i].overview,
          "cast": results[i].id,
          "genre": results[i].genres,
-         "vote": conversionVote(results[i].vote_average),
+         "stars_list": conversionVote(results[i].vote_average),
          "poster_path": SearchImage(results[i].poster_path),
          "original_language": language
      };
@@ -94,54 +96,16 @@ $(document).ready(function(){
       var vote = conversionVote(results[i].vote_average);
       //make an html variable with inside the handlebars context
       var html = template(context);
-      //append the stars inside the DOM
-      $("#series_list").append(vote);
       //append the html inside the DOM
-      $("#series_list").append(html);
+      if (type === "movie") {
+        $("#movies_list").append(html);
+      } else {
+        $("#series_list").append(html);
+      }
      }
     },
       "error": function (err) {
        alert("There is an error with the Ajax call. "+ err);
      }
   });
-  //make an ajax call for the movies
-  $.ajax(
-     {
-      "url": "https://api.themoviedb.org/3/search/movie",
-      "data":{
-        "api_key": apiKey,
-        "query": search,
-        "page": "1",
-     },
-       "method": "GET",
-       "success": function(data) {
-        var results = data.results;
-        //make a cicle for to get inside of the api Array objects
-        for (var i = 0; i < results.length; i++) {
-         var context = {
-         //make some property which are going to be inserted in the placeholders
-         "title": results[i].title || results[i].name,
-         "originaltitle": results[i].original_title || results[i].original_name,
-         "overview": results[i].overview,
-         "cast": results[i].id,
-         "genre": results[i].genres,
-         "vote": conversionVote(results[i].vote_average),
-         "poster_path": SearchImage(results[i].poster_path),
-         "original_language": language
-       }
-       var language = results[i].original_language;
-       //make a variable to take the vote results and convert it in stars value
-       var vote = conversionVote(results[i].vote_average);
-       //make an html variable with inside the handlebars context
-       var html = template(context);
-       //append the stars inside the DOM
-       $("#movies_list").append(vote);
-       //append the html inside the DOM
-       $("#movies_list").append(html);
-      }
-     },
-      "error": function (err) {
-       alert("There is an error with the Ajax call. "+ err);
-     }
-   });
 }
